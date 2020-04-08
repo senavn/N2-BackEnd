42b89cbc4ab793043009cd9a4a604486fe9d458d
unknown <vinicius.drum@hotmail.com>
2020-04-07 22:22:45 -0300

    add controller

---
+++ controllers/api/item.controller.js
@@ -0,0 +1,68 @@
+var config = require('config.json');
+var express = require('express');
+var router = express.Router();
+
+var itemService = require('services/item.service')
+
+// routes
+router.post('/register', registeritem);
+router.get('/:_id', getCurrentitem);
+router.put('/:_id', updateitem);
+router.delete('/:_id', deleteitem);
+router.get('/',getAllitem);
+
+module.exports = router;
+
+function getAllitem(req,res){
+    itemService.getAll()
+        .then(function (users) {
+            res.send(users);
+        })
+        .catch(function (err){
+            res.status(400).send(err);
+        });
+}
+
+function registeritem(req, res) {
+    itemService.create(req.body)
+        .then(function () {
+            res.sendStatus(200);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function getCurrentitem(req, res) {
+    itemService.getById(req.params._id)
+        .then(function (user) {
+            if (user) {
+                res.send(user);
+            } else {
+                res.sendStatus(404);
+            }
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function updateitem(req, res) {
+    itemService.update(req.params._id, req.body)
+        .then(function () {
+            res.sendStatus(200);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function deleteitem(req, res) {
+    itemService.delete(req.params._id)
+        .then(function () {
+            res.sendStatus(200);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
